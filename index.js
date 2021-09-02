3ffe1f2c29694aca89d4d97d86d6a9578c6e36ae
AnaelTorres <87792790+AnaelTorres@users.noreply.github.com>
2021-09-01 22:39:18 -0300

    Add files via upload
    asd

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const puerto = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, "./public")));
+const publicFolderPath = path.resolve(__dirname,"./public");
+
+app.use(express.static(publicFolderPath))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/index.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/login.html"));
+});
+
+app.listen(puerto,()=>{console.log('Server is runnig in the Port ' + puerto);
+  });
